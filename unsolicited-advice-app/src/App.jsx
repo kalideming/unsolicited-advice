d6a17f1beb84ce3aa2efae749ba64af17fb2b2c7
Kali Deming <kalideming@icloud.com>
2022-12-06 13:04:57 -0700

    home

--- unsolicited-advice-app/src/App.jsx
+++ unsolicited-advice-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Header from "./Header"
+import Header from "./Home"
 import AdvicePage from './AdvicePage'
 
 function App() {
