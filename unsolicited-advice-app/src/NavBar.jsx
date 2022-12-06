aa7a1c78bf0f993b5f2d685b5d0faa949c97de4d
Kali Deming <kalideming@icloud.com>
2022-12-06 13:32:47 -0700

    nav bar

--- unsolicited-advice-app/src/NavBar.jsx
+++ unsolicited-advice-app/src/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
-
+    return (
+        <header>
+            <nav>
+                <Link to="/">
+                    <h1 className="branding">
+                        Unsolicited Advice 
+                    </h1>
+                </Link>
+                <div className="navigation">
+                    <NavLink className="button" exact to="/advice">
+                        All The Unwanted Advice 
+                    </NavLink>
+                    <NavLink className="button" exact to="/advice/new">
+                        Add Unwanted Advice 
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default NavBar;
--- unsolicited-advice-app/src/main.jsx
+++ unsolicited-advice-app/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom' // /client removed
 import App from './App'
-import './index.css'
+// import './index.css'
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
-)
+  </Router>
+);
