f38862e65fc7b5166a648b4f0603fa3f457ebf9f
Kali Deming <kalideming@icloud.com>
2022-12-07 12:18:16 -0700

    added Tailwind

---
+++ unsolicited-advice-app/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/AdviceCard.jsx",
+    "./src/AdviceList.jsx",
+    "./src/AdvicePage.jsx",
+    "./src/App.jsx",
+    "./src/Home.jsx",
+    "./src/main.jsx",
+    "./src/NavBar.jsx",
+    "./src/NewAdvicePage.jsx",
+    "./src/Search.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
