a1018c5a5ee81c7c97b18e585cdded49472852c9
Kali Deming <kalideming@icloud.com>
2022-12-07 21:01:13 -0700

    wow I hate CSS

--- unsolicited-advice-app/src/App.jsx
+++ unsolicited-advice-app/src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route } from "react-router-dom"
-// import reactLogo from './assets/react.svg'
-// import './App.css'
 import Home from "./Home";
 import AdvicePage from './AdvicePage';
 import NavBar from './NavBar';
 import NewAdvicePage from './NewAdvicePage';
+
 
 function App() {
 
